ae0a7579f9ea17ad93a572bd00684bc91a86a32e
Annie-Huang <annie.hn@gmail.com>
2019-10-21 21:25:46 +1100

    Enable onSubmit to handle both add and update. And change binding to editingPost.title and editingPost.body so we can edit the input fields

--- src/components/PostForm.svelte
+++ src/components/PostForm.svelte
@@ -24,8 +24,17 @@
 
         const newPost = {title, body};
 
-        const res = await fetch(`${apiBaseUrl}/post`, {
-            method: 'POST',
+        let url, method;
+        if(editingPost.id) {
+            url = `${apiBaseUrl}/post/${editingPost.id}`;
+            method = 'PUT';
+        } else {
+            url = `${apiBaseUrl}/post`;
+            method = 'POST';
+        }
+
+        const res = await fetch(url, {
+            method,
             body: JSON.stringify(newPost)
         });
         const post = await res.json();
@@ -49,11 +58,11 @@
     <form on:submit={onSubmit}>
         <div class="input-field">
             <label for="title">Title</label>
-            <input type="text" bind:value={title}/>
+            <input type="text" bind:value={editingPost.title}/>
         </div>
         <div class="input-field">
             <label for="body">Body</label>
-            <input type="text" bind:value={body}/>
+            <input type="text" bind:value={editingPost.body}/>
         </div>
         <button type="submit" class="waves-effect waves-light btn">
             {editingPost.id ? 'Update' : 'Add'}
