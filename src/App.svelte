cf99cf68a84fa0da9e23366eaa83a1aaa3f1ad83
Annie-Huang <annie.hn@gmail.com>
2019-10-21 18:00:58 +1100

    Create routing and linked to empty components for now.

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,20 @@
 <script>
     import '../node_modules/materialize-css/dist/css/materialize.css';
     import '../node_modules/materialize-css/dist/js/materialize.js';
+
+    import { Router, Link, Route } from "svelte-routing";
+
+    import Navbar from './layout/Navbar.svelte';
 </script>
 
 <style>
 
 </style>
 
-<h1>Hello {name}!</h1>
+<Router>
+    <Navbar />
+    <div class="container">
+        <Route path="/" component={Home}></Route>
+        <Route path="/about" component={About}></Route>
+    </div>
+</Router>
---
+++ src/layout/Navbar.svelte
@@ -0,0 +1,14 @@
+<script>
+    import { Link } from "svelte-routing";
+</script>
+
+<!--From https://materializecss.com/navbar.html-->
+<nav>
+    <div class="nav-wrapper">
+        <Link to="/" class="brand-logo">Svelte Tutorial</Link>
+        <ul id="nav-mobile" class="right hide-on-med-and-down">
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+        </ul>
+    </div>
+</nav>
