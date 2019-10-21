64c0a12ed60270edfd02eaad27d379a82f4b860b
Annie-Huang <annie.hn@gmail.com>
2019-10-21 20:37:20 +1100

    Adding loading logic to show loading bar while submitting the form to server

--- src/components/PostForm.svelte
+++ src/components/PostForm.svelte
@@ -1,6 +1,7 @@
 <script>
     let title = '';
     let body = '';
+    let loading = false;
 
     const apiBaseUrl = "https://ndb99xkpdk.execute-api.eu-west-2.amazonaws.com/dev";
 
@@ -11,6 +12,8 @@
             return;
         }
 
+        loading = true;
+
         const newPost = {title, body};
 
         const res = await fetch(`${apiBaseUrl}/post`, {
@@ -18,21 +21,33 @@
             body: JSON.stringify(newPost)
         });
         const post = await res.json();
+        loading = false;
     }
 </script>
 
 <style>
-
+    form {
+        margin: 50px;
+    }
+    .progress {
+        margin: 100px 0;
+    }
 </style>
 
-<form on:submit={onSubmit}>
-    <div class="input-field">
-        <label for="title">Title</label>
-        <input type="text" bind:value={title} />
+{#if !loading}
+    <form on:submit={onSubmit}>
+        <div class="input-field">
+            <label for="title">Title</label>
+            <input type="text" bind:value={title}/>
+        </div>
+        <div class="input-field">
+            <label for="body">Body</label>
+            <input type="text" bind:value={body}/>
+        </div>
+        <button type="submit" class="waves-effect waves-light btn">Add</button>
+    </form>
+{:else}
+    <div class="progress">
+        <div class="indeterminate" />
     </div>
-    <div class="input-field">
-        <label for="body">Body</label>
-        <input type="text" bind:value={body} />
-    </div>
-    <button type="submit" class="waves-effect waves-light btn">Add</button>
-</form>
+{/if}
